a0a6d53ccce726e130e0ebc63813efebd105ea6d
Sangjun Lee <lsjmc0224@dgu.edu>
2023-11-10 02:40:18 +0900

    :sparkles: Added bad words filtering

--- main/views.py
+++ main/views.py
@@ -11,6 +11,27 @@ from .models import *
 from .serializers import *
 from django.http import Http404
 
+# 욕설 필터링 관련 import
+from django.conf import settings
+import os
+
+def load_swears(file_name):
+    file_path = os.path.join(settings.BASE_DIR, 'static', file_name)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        swears = file.read().splitlines()
+    return swears
+SWEARS = load_swears('fword_list.txt')
+
+def censor_content(content):
+    is_abused = False
+    
+    for swear in SWEARS:
+        if swear in content:
+            content = content.replace(swear, '*' * len(swear))
+            is_abused = True
+            
+    return content, is_abused
+
 class QuestionViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
     # api/questoin/으로 GET 요청을 받을 시, 랜덤 질문 1개를 반환함
     queryset = Question.objects.all()
@@ -50,10 +71,12 @@ class AnswerCreateViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
     queryset = Answer.objects.all()
     serializer_class = AnswerCreateSerializer
 
+    # censor_content를 이용한 욕설 필터링. 욕설이 포함되어 있으면 _ 가 True로 반환됨
     def create(self, request, *args, **kwargs):
-        # user id 없이 답변을 생성할 수 있도록 함
         serializer = AnswerCreateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        content = serializer.validated_data['content']
+        content, _ = censor_content(content)
+
+        serializer.validated_data['content'] = content
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
