20953501b62cad724d0d3d52435b2fb272b75036
chaeunho1227 <chaeunho2002@gmail.com>
2023-11-09 14:06:39 +0900

    ✨ feat : set models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+class Question(models.Model):
+    CATEGORY_LIST = (
+        ('Past', 'Past'),
+        ('Present', 'Present'),
+        ('Future', 'Future'),
+    )
+    id = models.AutoField(primary_key=True)
+    content = models.CharField(null=False, blank=False, max_length=100)
+    category = models.CharField(max_length=10, choices=CATEGORY_LIST, blank=False, null=False)
+
+class Answer(models.Model):
+    id = models.AutoField(primary_key=True)
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    writer = models.CharField(null=False, blank=False, max_length=30)
+    content = models.TextField(null=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
+# accounts/model.py
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, password=None):
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password):
+        user = self.create_user(
+            email,
+            nickname=nickname,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=100,
+        unique=True,
+    )
+    nickname = models.CharField(max_length=30, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname']
+
+    def __str__(self):
+        return self.email
