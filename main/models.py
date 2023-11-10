481cbb6b150fab7feb32634172b2b934c3b914a5
chaeunho1227 <chaeunho2002@gmail.com>
2023-11-11 02:20:44 +0900

    🩹 admin 페이지 모델 표시 수정

--- main/models.py
+++ main/models.py
@@ -10,6 +10,9 @@ class Question(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.CharField(null=False, blank=False, max_length=100)
     category = models.CharField(max_length=10, choices=CATEGORY_LIST, blank=False, null=False)
+    
+    def __str__(self):
+        return str(self.id) + "번 '" + self.category + "' 질문 : " + self.content
 
 class Answer(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,3 +21,6 @@ class Answer(models.Model):
     content = models.TextField(null=False)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.question.id) + "번 질문 답변 : " + self.content + "by" + self.writer
